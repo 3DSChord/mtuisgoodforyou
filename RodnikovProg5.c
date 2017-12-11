f1268ade44bbd10121cd4091b274d71abd27b6a2
3DSChord <31824652+3DSChord@users.noreply.github.com>
2017-12-11 23:06:29 +0400

    Add files via upload

---
+++ RodnikovProg5.c
@@ -0,0 +1,156 @@
+/* ѕрактическа€ работа є5 */
+/* ћашина “ьюринга. */
+/* —тудент: –одников »ль€ */
+/* √руппа: » Ѕќ-05-17 */
+/* Language: C */
+/* Windows compiler: Microsoft Visual C++ 2010 Express */
+/* Linux compiler: gcc -std=c89 -Wall -Werror -Wextra */
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h"
+
+typedef struct {
+	int startCell;
+	int headPos;
+	int headValue;
+	int *leftArray;
+	size_t leftUsed;
+	size_t leftSize;
+	int *rightArray;
+	size_t rightUsed;
+	size_t rightSize;
+} Tape;
+
+void trim (unsigned char *cs);
+void initTape(Tape *a, size_t initialSize);
+void freeTape(Tape *a);
+void incTape(Tape *a);
+
+int main(void)
+{
+	char c;
+	FILE *ptr_file;
+	unsigned char buf[1000];
+	Tape b;
+
+	printf("----- TURING COMPUTER -----\n");
+
+	initTape(&b, 1000);
+
+	ptr_file =fopen("input.txt","r");
+	if (!ptr_file)
+		return 1;
+
+	while (fgets((char *)buf,1000, ptr_file)!=NULL) {
+		trim(buf);
+		if (buf[0] != '\0') {
+			printf("%s\n",buf);
+			if (strcmp((char *)buf, "movl") == 0) 
+			{
+				printf("переход влево на одну €чейку\n");
+			} 
+			else if (strcmp((char *)buf, "movr") == 0)
+			{
+				printf("переход вправо на одну €чейку\n");
+			}
+			/* more else if clauses */
+			else if (strcmp((char *)buf, "inc") == 0)
+			{
+				printf("инкремент значени€ по текущему адресу\n");
+				incTape(&b);
+				printf("%d\n", b.headValue);
+			}
+			else if (strcmp((char *)buf, "dec") == 0)
+			{
+				printf("декремент значени€ по текущему адресу\n");
+			}
+			else if (strcmp((char *)buf, "print") == 0)
+			{
+				printf("вывод значени€ по текущему адресу на экран\n");
+			}
+			else if (strcmp((char *)buf, "get") == 0)
+			{
+				printf("считать 1 байт с клавиатуры и записать в текущую €чейку\n");
+			}
+			else if (strcmp((char *)buf, "printc") == 0)
+			{
+				printf("вывод значени€ текущей €чейки как ASCII-символа (например, 65 должно быть выведено как A)\n");
+			}
+			else if (strcmp((char *)buf, "begin") == 0)
+			{
+				printf("переход на следующую за соответствующей инструкцией end инструкцию, если в текущей €чейке записан 0\n");
+			}
+			else if (strcmp((char *)buf, "end") == 0)
+			{
+				printf("если в текущей €чейке не 0, то переход обратно на инструкцию, следующую за соответствующей инструкцией begin\n");
+			}
+			else /* default: */
+			{
+				if (buf[0] == '*') printf("строка-комментарий\n");
+				else printf("неизвестна€ команда\n");
+			}
+		}
+		buf[0] = '\0';
+	}
+
+	fclose(ptr_file);
+
+	freeTape(&b);
+	clearerr(stdin);
+	while ((c = getchar()) != '\n') {};
+	return 0;
+}
+
+void trim (unsigned char *cs)
+{
+	unsigned char *p;
+	size_t len = strlen((char *)cs);
+	for (p = cs + len - 1; isspace (*p); --p) /* nothing */ ;
+	p[1] = '\0';
+	for (p = cs; isspace (*p); ++p) /* nothing */ ;
+	memmove (cs, p, len - (size_t) (p - cs) + 1);
+}
+
+void initTape(Tape *a, size_t initialSize) {
+	a->startCell = 0;
+	a->headPos = 0;
+	a->headValue = 0;
+	a->leftArray = (int *)malloc(initialSize * sizeof(int));
+	a->leftUsed = 0;
+	a->leftSize = initialSize;
+	a->rightArray = (int *)malloc(initialSize * sizeof(int));
+	a->rightUsed = 0;
+	a->rightSize = initialSize;
+}
+
+void freeTape(Tape *a) {
+	free(a->leftArray);
+	a->leftArray = NULL;
+	a->leftUsed = 0;
+	a->leftSize = 0;
+	free(a->rightArray);
+	a->rightArray = NULL;
+	a->rightUsed = 0;
+	a->rightSize = 0;
+	a->startCell = 0;
+	a->headPos = 0;
+	a->headValue = 0;
+}
+
+void incTape(Tape *a) {
+if (a->headPos == 0) {
+	a->startCell++;
+	a->headValue = a->startCell;
+}
+if (a->headPos > 0) {
+	a->headValue++;
+	a->rightArray[a->headPos-1]++;
+}
+if (a->headPos < 0) {
+	a->headValue++;
+	a->leftArray[-(a->headPos+1)]++;
+}
+}
+
