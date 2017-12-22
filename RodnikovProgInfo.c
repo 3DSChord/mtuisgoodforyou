c5039f9af0954fe369d8beaaabf527199ffaff84
3DSChord <31824652+3DSChord@users.noreply.github.com>
2017-12-22 18:09:00 +0300

    Update RodnikovProgInfo.c

--- RodnikovProgInfo.c
+++ RodnikovProgInfo.c
@@ -9,9 +9,14 @@ int main(){
 	int c[2];
 	srand(123);
 	choice = '\0';
-	printf("----- ENTER NUMBER OF ELEMENTS -----\n");
-	scanf("%d", &n);
-	while ((t = getchar()) != '\n') {};
+	n = 0;
+	while((n < 4) || (n > 100)) {
+		printf("----- ENTER NUMBER OF ELEMENTS -----\n");
+		scanf(" %d", &n);
+		while ((t = getchar()) != '\n') {};
+		if((n < 4) || (n > 100)) printf("Entered value %d is not valid.\n", n);
+	}
+	printf("Array of %d elements is to be filled.\n", n);
 	while ((choice != 'Q') && (choice != 'q')) {
 		printf("----- ENTER <<A>> FOR AUTO-INPUT OR <<M>> FOR MANUAL, <<Q>> TO QUIT -----\n");
 		/* choice = getchar(); */
@@ -22,10 +27,6 @@ int main(){
 			{
 				a[i] = 0 + rand() % 100;
 			}
-			for(i = 0; i<n; i++)
-			{
-				printf("%d\n", a[i]);
-			}
 		}
 		else if (choice == char_M){
 			for (i = 0; i<n; i++) 
@@ -33,12 +34,14 @@ int main(){
 				printf("a[%d] = ", i);
 				scanf(" %d", &a[i]);
 			}
-			for(i = 0; i<n; i++)
-			{
-				printf("%d\n", a[i]);
-			}
 		}
 		if((choice == char_A) || (choice == char_M)) {
+			printf("----- ARRAY OF %d ELEMENTS -----\n", n);
+			for(i = 0; i<n; i++)
+			{
+				printf("%d ", a[i]);
+			}
+			printf("\nSorting the array ...\n");
 			for(i = 0; i < n-1; i++) { 
 				for(j = 0; j < n - 1 - i; j++) { 
 					if(a[j] > a[j+1]) { 
@@ -58,12 +61,13 @@ int main(){
 		printf("----- ENTER N-th MAX AND M-th MIN OF %d ELEMENTS -----\n", n);
 		scanf(" %d %d", &N, &M);
         while ((t = getchar()) != '\n') {};
+		if((N < 1) || (N > n)) printf("Invalid value N = %d\n", N);
+		if((M < 1) || (M > n)) printf("Invalid value M = %d\n", M);
 	}
 	c[0] = a[M - 1];
 	c[1] = a[n - N];
 	printf("max = %d\n", c[1]);
 	printf("min = %d\n", c[0]);
-	while ((choice = getchar()) != '\n') {};
 /*	for(i = 0; i<n; i++)
 	{
 		if ((i != M-1) && (i != n - N))
@@ -76,11 +80,12 @@ int main(){
 			if (i == n - N) c[1] = a[i];
 		}
 	}*/
+	printf("----- ARRAY OF %d ELEMENTS -----\n", ((M - 1) != (n - N)) ? n - 2:n - 1);
 	for (i = 0; i<n; i++){
 		if((i != M - 1) && (i != n - N)) printf("%d ", a[i]);
 	}
 	printf("\n");
-	printf("%d-th MAX = %d, %d-th MIN = %d\n", N, c[1], M, c[0]);
+	printf("----- EXCLUDED %d-th MAX = %d AND %d-th MIN = %d -----\n", N, c[1], M, c[0]);
 /*	for (i = 0; i<2; i++){
 		printf("%d ", c[i]);
 	} */
@@ -88,4 +93,3 @@ int main(){
 	while ((choice = getchar()) != '\n') {};
 	return 0;
 }
-
